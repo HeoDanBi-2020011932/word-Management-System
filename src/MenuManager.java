d8fcb7ac3499687022f9e0ef200a2563fe01ab22
HeoDanBi-2020011932 <danbi4331@gnu.ac.kr>
2021-03-21 23:37:36 +0900

    add menu and case 1

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
 
 public class MenuManager {
-
 	public static void main(String[] args) {
-		System.out.println("1. Add words ");
-		System.out.println("2. Delete words ");
-		System.out.println("3. Edit words ");
-		System.out.println("4. View words ");
-		System.out.println("5. Exit ");
-
+		int num=0;
+		Scanner input = new Scanner(System.in);
+		
+		while(num !=6) {
+			System.out.println("1. Add words ");
+			System.out.println("2. Delete words ");
+			System.out.println("3. Edit words ");
+			System.out.println("4. View words ");
+			System.out.println("5. Show the menu ");
+			System.out.println("6. Exit ");
+			System.out.println("Select one number between 1-6");
+			
+			num=input.nextInt();
+			
+			switch(num) {
+			case 1:
+				System.out.print("memory word: ");
+				String word = input.next();
+				break;
+			case 2:
+			case 3:
+			case 4:
+			}
+		}
 	}
-
 }
+
