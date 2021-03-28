a819a4770d9bcb4ae4166b60108266c347476886
HeoDanBi-2020011932 <danbi4331@gnu.ac.kr>
2021-03-28 19:35:31 +0900

    modified case to method (1-4).

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -6,6 +6,7 @@ public class MenuManager {
 		Scanner input = new Scanner(System.in);
 		
 		while(num !=6) {
+			System.out.println("бщ My Word Manager Menu бщ");
 			System.out.println("1. Add words ");
 			System.out.println("2. Delete words ");
 			System.out.println("3. Edit words ");
@@ -18,14 +19,44 @@ public class MenuManager {
 			
 			switch(num) {
 			case 1:
-				System.out.print("memory word: ");
-				String word = input.next();
+				Addwords();
 				break;
 			case 2:
+				Deletewords();
+				break;
 			case 3:
+				Editwords();
+				break;
 			case 4:
+				Viewwords();
+				break;
 			}
 		}
+	}
+	public static void Addwords() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("memory word: ");
+		String word = input.next();
+		System.out.print("word class: ");
+		String wordclass = input.next();
+		System.out.print("meaning: ");
+		String mean = input.next();
+		}
+	
+	public static void Deletewords() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("memory word: ");
+		String word = input.next();
+	}
+	public static void Editwords() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("memory word: ");
+		String word = input.next();
+	}
+	public static void Viewwords() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("memory word: ");
+		String word = input.next();
 	}
 }
 
