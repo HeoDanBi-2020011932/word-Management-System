5ad6694977d95e641528ec081e9298f559805ef6
HeoDanBi-2020011932 <danbi4331@gnu.ac.kr>
2021-05-10 18:11:35 +0900

    changed Word to WordInput

--- src/WordManager.java
+++ src/WordManager.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import word.English;
 import word.Japanese;
 import word.Korean;
 import word.Word;
+import word.WordInput;
 import word.Wordkind;
 
 public class WordManager {
-	ArrayList<Word> words = new ArrayList<Word>();
+	ArrayList<WordInput> words = new ArrayList<WordInput>(); //WordInput으로 변경
 	Scanner input;
 	WordManager(Scanner input){
 		this.input=input;
@@ -15,7 +17,7 @@ public class WordManager {
 	
 	public void Addword() {
 		int kind=0;
-		Word word;
+		WordInput wordInput;
 		while(kind!=1 && kind!=2) {
 		System.out.println("1.English");
 		System.out.println("2.Japanese");
@@ -23,21 +25,21 @@ public class WordManager {
 		System.out.println("Select num 1,2 or 3 for language kind: ");
 		kind =input.nextInt();
 		if (kind==1) {
-			word = new Word(Wordkind.English); 		//새 객체 생성
-			word.getWordInput(input);
-			words.add(word); 		//항목 추가
+			wordInput = new English(Wordkind.English); 		//새 객체 생성
+			wordInput.getWordInput(input);
+			words.add(wordInput); 		//항목 추가
 			break;
 		}
 		else if(kind==2){
-			word = new Japanese(Wordkind.Japanese);		//새 객체 생성
-			word.getWordInput(input);
-			words.add(word); 		//항목 추가
+			wordInput = new Japanese(Wordkind.Japanese);		//새 객체 생성
+			wordInput.getWordInput(input);
+			words.add(wordInput); 		//항목 추가
 			break;
 		}
 		else if(kind==3){
-			word=new Korean(Wordkind.Korean);	//새 객체 생성
-			word.getWordInput(input);
-			words.add(word); 		//항목 추가
+			wordInput=new Korean(Wordkind.Korean);	//새 객체 생성
+			wordInput.getWordInput(input);
+			words.add(wordInput); 		//항목 추가
 			break;
 		}
 		else {
@@ -64,13 +66,13 @@ public class WordManager {
 			System.out.println("the word has not been registered");
 			return;
 		}
-	} // 저번주는 하나만 찾을 수 있었다면 이번에는 배열 전체를 돌아보는 것
+	}
 	 
 	public void Editword() {
 		System.out.print("memory word: ");
 		String word1 = input.next();
 		for(int i=0;i<words.size();i++) {
-			Word word =words.get(i);
+			WordInput wordInput =words.get(i);
 			if (words.get(i).getMemorzied_word().equals(word1)) {
 				int num=-1;
 				while(num !=5) {
@@ -87,22 +89,22 @@ public class WordManager {
 					case 1:
 						System.out.println("word in menory: "); 
 						String memorzied_word=input.next();
-						word.setMemorzied_word(memorzied_word);
+						wordInput.setMemorzied_word(memorzied_word);
 						break;
 					case 2:
 						System.out.println("word class: ");
 						String wordclass=input.next();
-						word.setWordclass(wordclass);
+						wordInput.setWordclass(wordclass);
 						break;
 					case 3:
 						System.out.println("meaning of word: ");
 						String mean=input.next();
-						word.setMean(mean);
+						wordInput.setMean(	mean);
 						break;
 					case 4:
 						System.out.println("pronunciation: ");
 						String pronun=input.next();
-						((Japanese) word).setAnswer(pronun);
+						((Japanese) wordInput).setAnswer(pronun);
 						}
 						break;
 					}//switch
