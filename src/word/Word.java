ce392501e61add701ca54df760d3ffacbab3d7e2
HeoDanBi-2020011932 <danbi4331@gnu.ac.kr>
2021-05-10 18:12:30 +0900

    changed abstract class

--- src/word/Word.java
+++ src/word/Word.java
@@ -2,7 +2,7 @@ package word;
 
 import java.util.Scanner;
 
-public class Word {
+public abstract class Word {
 	protected Wordkind kind=Wordkind.English;
 	protected String memorzied_word;
 	protected String wordclass;
@@ -59,39 +59,7 @@ public class Word {
 		this.mean = mean;
 	}
 	
-	public void printInfo() {
-		String skind="none";
-		switch(this.kind) {
-		case English:
-			skind="eng";
-			break;
-		case Japanese:
-			skind="jp";
-			break;
-		case Korean:
-			skind="kr";
-			break;
-		case Chinese:
-			skind="ch";
-			break;
-		default:
-			
-		}
-		System.out.println("kind:"+skind+" memorzied_word: "+memorzied_word+" wordclass: "+wordclass+" mean: "+mean);
-	}
-
-	public void getWordInput(Scanner input) {
-		System.out.print("word in memory: ");
-		String memorzied_word = input.next();
-		this.setMemorzied_word(memorzied_word);
-		
-		System.out.print("word class: ");
-		String wordclass = input.next();
-		this.setWordclass(wordclass);
-		
-		System.out.print("meaning: ");
-		String mean = input.next();
-		this.setMean(mean);
-	}
+	public abstract void printInfo();
+	
 
 }
