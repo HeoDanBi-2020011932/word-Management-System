fe64db8ea227cdabf1a5c19e1e225c6a9c6f4bac
HeoDanBi-2020011932 <danbi4331@gnu.ac.kr>
2021-05-18 01:29:40 +0900

    refactoring code

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,37 +1,53 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuManager {
 	public static void main(String[] args) {
-		int num=0;
 		Scanner input = new Scanner(System.in);
 		WordManager wordManager = new WordManager(input);
-		
+		selectmenu(input,wordManager);
+	}
+	
+	
+	public static void selectmenu(Scanner input, WordManager wordManager){
+		int num=-1;
 		while(num !=5) {
-			System.out.println("бщ My Word Manager Menu бщ");
-			System.out.println("1. Add words ");
-			System.out.println("2. Delete words ");
-			System.out.println("3. Edit words ");
-			System.out.println("4. View words ");
-			System.out.println("5. Exit ");
-			System.out.println("Select one number between 1-5");
-			
-			num=input.nextInt();
-			
-			switch(num) {
-			case 1:
-				wordManager.Addwords();
-				break;
-			case 2:
-				wordManager.Deletewords();
-				break;
-			case 3:
-				wordManager.Editwords();
-				break;
-			case 4:
-				wordManager.Viewwords();
-				break;
+			try {
+				showmenu();
+				num=input.nextInt();
+				switch(num) {
+				case 1:
+					wordManager.Addword();
+					break;
+				case 2:
+					wordManager.Deleteword();
+					break;
+				case 3:
+					wordManager.Editword();
+					break;
+				case 4:
+					wordManager.Viewwords();
+					break;
+				default:
+					continue;
+				}
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Please put an integer between 1 and 5! ");
+				if(input.hasNext())
+					input.next();
+				num=-1;
+			}
 			}
 		}
+	public static void showmenu() {
+		System.out.println("бщ My Word Manager Menu бщ");
+		System.out.println("1. Add word ");
+		System.out.println("2. Delete word ");
+		System.out.println("3. Edit word ");
+		System.out.println("4. View words ");
+		System.out.println("5. Exit ");
+		System.out.println("Select one number between 1-5");
 	}
 
 }
--- src/WordManager.java
+++ src/WordManager.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import word.English;
@@ -18,95 +19,98 @@ public class WordManager {
 	public void Addword() {
 		int kind=0;
 		WordInput wordInput;
-		while(kind!=1 && kind!=2) {
-		System.out.println("1.English");
-		System.out.println("2.Japanese");
-		System.out.println("3. Korean");
-		System.out.println("Select num 1,2 or 3 for language kind: ");
-		kind =input.nextInt();
-		if (kind==1) {
-			wordInput = new English(Wordkind.English); 		//새 객체 생성
-			wordInput.getWordInput(input);
-			words.add(wordInput); 		//항목 추가
-			break;
-		}
-		else if(kind==2){
-			wordInput = new Japanese(Wordkind.Japanese);		//새 객체 생성
-			wordInput.getWordInput(input);
-			words.add(wordInput); 		//항목 추가
-			break;
-		}
-		else if(kind==3){
-			wordInput=new Korean(Wordkind.Korean);	//새 객체 생성
-			wordInput.getWordInput(input);
-			words.add(wordInput); 		//항목 추가
-			break;
-		}
-		else {
-			System.out.println("Select num for language kind");
+		while(kind<1 || kind>	3) {
+			try {
+				System.out.println("1.English");
+				System.out.println("2.Japanese");
+				System.out.println("3. Korean");
+				System.out.println("Select num 1,2 or 3 for language kind: ");
+				kind =input.nextInt();
+				if (kind==1) {
+					wordInput = new English(Wordkind.English); 		//새 객체 생성
+					wordInput.getWordInput(input);
+					words.add(wordInput); 		//항목 추가
+					break;
+				}
+				else if(kind==2){
+					wordInput = new Japanese(Wordkind.Japanese);		//새 객체 생성
+					wordInput.getWordInput(input);
+					words.add(wordInput); 		//항목 추가
+					break;
+				}
+				else if(kind==3){
+					wordInput=new Korean(Wordkind.Korean);	//새 객체 생성
+					wordInput.getWordInput(input);
+					words.add(wordInput); 		//항목 추가
+					break;
+				}
+				else {
+					System.out.println("Select num for language kind");
+					}
+				}
+			catch(InputMismatchException e) {
+				System.out.println("Please put an integer between 1 and 3! ");
+				if(input.hasNext())
+					input.next();
+				kind=-1;
 			}
-		}
-	}	
+		 }
+	}
 	public void Deleteword() {
 		System.out.print("memory word: ");
 		String word1 = input.next();
-		
+		int index = findIndex(word1);
+		removefromWord(index, word1);
+		} 	
+	
+	public int findIndex(String word) {
 		int index = -1; // -1이다, 배열에서 인덱스를 찾지 못한 것
 		for(int i=0;i<words.size();i++) {
-			if (words.get(i).getMemorzied_word().equals(word1)) {
+			if (words.get(i).getMemorzied_word().equals(word)) {
 				index = i;
 				break;
 			}
-		}
+		}	
+		return index;
+	}
+	
+	public int removefromWord(int index, String word) {
 		if(index>=0) {
 			words.remove(index);
-			System.out.println("the word "+word1+" is deleted");
+			System.out.println("the word "+word+" is deleted");
+			return 1; 
 		}
 		else {
 			System.out.println("the word has not been registered");
-			return;
+			return -1;
 		}
 	}
-	 
 	public void Editword() {
 		System.out.print("memory word: ");
 		String word1 = input.next();
 		for(int i=0;i<words.size();i++) {
-			WordInput wordInput =words.get(i);
+			WordInput word =words.get(i);
 			if (words.get(i).getMemorzied_word().equals(word1)) {
 				int num=-1;
 				while(num !=5) {
-					System.out.println("☆ My Word Manager Menu ☆");
-					System.out.println("1. Edit word ");
-					System.out.println("2. Edit word class");
-					System.out.println("3. Edit meaning ");
-					System.out.println("4. Edit pronunciation ");
-					System.out.println("5. Exit ");
-					System.out.println("Select one number between 1-5");
-			
+					showEditMenu();
 					num=input.nextInt();
 					switch(num) {
 					case 1:
-						System.out.println("word in menory: "); 
-						String memorzied_word=input.next();
-						wordInput.setMemorzied_word(memorzied_word);
+						word.setWord(input);
 						break;
 					case 2:
-						System.out.println("word class: ");
-						String wordclass=input.next();
-						wordInput.setWordclass(wordclass);
+						word.setWordClass(input);
 						break;
 					case 3:
-						System.out.println("meaning of word: ");
-						String mean=input.next();
-						wordInput.setMean(	mean);
+						word.setWordMean(input);
 						break;
 					case 4:
-						System.out.println("pronunciation: ");
-						String pronun=input.next();
-						((Japanese) wordInput).setAnswer(pronun);
-						}
+						((Japanese) word).setWordPronunce(input);
 						break;
+					default:
+						continue;
+						}
 					}//switch
 				}//while
 			}//if
@@ -119,5 +123,14 @@ public class WordManager {
 		for(int i=0;i<words.size();i++) {
 			words.get(i).printInfo(); //각각의 인덱스를 가지고 온다
 		}				
+	}	
+	public void showEditMenu() {
+		System.out.println("☆ My Word Manager Menu ☆");
+		System.out.println("1. Edit word ");
+		System.out.println("2. Edit word class");
+		System.out.println("3. Edit meaning ");
+		System.out.println("4. Edit pronunciation ");
+		System.out.println("5. Exit ");
+		System.out.println("Select one number between 1-5");
 	}
 }
