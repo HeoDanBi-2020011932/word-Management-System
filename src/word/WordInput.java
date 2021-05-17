7404f254a39b6d8420515d39c4370f357b16f2e9
HeoDanBi-2020011932 <danbi4331@gnu.ac.kr>
2021-05-18 01:35:00 +0900

    add setMemorzied_word(String memorzied_word) throws WordFormatException

--- src/word/WordInput.java
+++ src/word/WordInput.java
@@ -2,12 +2,17 @@ package word;
 
 import java.util.Scanner;
 
+import exception.WordFormatException;
+
 public interface WordInput {
 	public void getWordInput(Scanner input);
 	public String getMemorzied_word();
-	public void setMemorzied_word(String memorzied_word);
+	public void setMemorzied_word(String memorzied_word) throws WordFormatException;
 	public void setWordclass(String wordclass);
 	public void setMean(String mean);
 	public void printInfo();
-
+	public void setWord(Scanner input);
+	public void setWordClass(Scanner input);
+	public void setWordMean(Scanner input);
+	
 }
